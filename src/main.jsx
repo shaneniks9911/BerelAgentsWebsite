2fccb6075364d8e8cf5b5bad25fdd364890790b5
shaneniks9911 <shaneniks99@gmail.com>
2025-12-02 04:23:04 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Website from './Website.jsx' // We changed App to Website here
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Website />
   </React.StrictMode>,
 )
